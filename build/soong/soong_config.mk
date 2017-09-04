622360218679e15662b0bc4fba38df51a8be5a27
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put wave variables in the struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Lineage structure
 wave_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Wave": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
