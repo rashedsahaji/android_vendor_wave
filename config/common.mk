ArrowOS/android_vendor_arrow

vendor: update init rc file copy path
Change-Id: I93cb77d20590fc005aa2e82e97d9a4584a15b64c

config/common.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # init file
 PRODUCT_COPY_FILES += \
-    vendor/arrow/prebuilt/common/etc/init.local.rc:root/init.arrow.rc
+    vendor/arrow/prebuilt/common/etc/init.local.rc:system/etc/init/init.arrow.rc
 
 # Backup Tool
 PRODUCT_COPY_FILES += \

rashedsahaji/android_vendor_wave

config/common.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # init file
 PRODUCT_COPY_FILES += \
-    vendor/wave/prebuilt/common/etc/init.local.rc:root/init.wave.rc
+    vendor/wave/prebuilt/common/etc/init.local.rc:system/etc/init/init.arrow.rc
 
 # Backup Tool
 PRODUCT_COPY_FILES += \