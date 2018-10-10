ArrowOS/android_vendor_arrow

vendor: Add Pixel's sysconfigs
Change-Id: I17fb14b9cdad71aa39986b799f062e202daaacad

config/common.mk
@@ -55,6 +55,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Bootanimation
 include vendor/arrow/config/bootanimation.mk
 

rashedsahaji/android_vendor_wave

config/common.mk
@@ -55,6 +55,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/wave/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Bootanimation
 include vendor/wave/config/bootanimation.mk
 